6a5b2824208715bdb55ee503c8f7539812cb3396
Devin <devin@blackhat.co.za>
2018-12-24 12:25:14 +0200

    missing files

---
+++ src/BinListServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace DevinPearson\BinList;
+
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * This is the BinList service provider class.
+ */
+class BinListServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->registerBindings();
+    }
+
+    /**
+     * Register the factory class.
+     *
+     * @return void
+     */
+    protected function registerBindings(): void
+    {
+        $this->app->singleton('binlist', function () {
+            return new BinList();
+        });
+
+        $this->app->alias('binlist', BinList::class);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return string[]
+     */
+    public function provides(): array
+    {
+        return [
+            'binlist',
+        ];
+    }
+}
---
+++ src/Facades/BinList.php
@@ -0,0 +1,22 @@
+<?php
+namespace DevinPearson\BinList\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * This is the BinList facade class.
+ *
+ * @method static array check(string $binNumber) fetches the binlist details.
+ */
+class BinList extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return 'binlist';
+    }
+}
